ab0d740a2841c6855e3fb90e3ee0d843aed1ec76
Thomas Li Fredriksen <thomas.fredriksen@oceandata.earth>
2023-05-04 15:16:45 +0200

    Updated README. Removed some dead code

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Agent Chain
+
+Agent Chain is a simple CLI built on top of [LangChain](https://python.langchain.com/en/latest/index.html) for
+running AI agents. It is meant for testing and experimenting with different AI agents.
+
+
+## Dependencies
+
+- Poetry
+- Docker
+
+
+## Getting started
+
+Start a Redis-search instance:
+
+```shell
+docker run --name redis-baby-agi -p 6379:6379 -d redislabs/redisearch
+```
+
+Enter the poetry shell in order to access the CLI
+```shell
+poetry shell
+python -m agent_chain --help
+```
+
+In order to use the BabyAGI or AutoGPT commands, the environment-variable `OPENAI_API_KEY` must be set. For convenience,
+you can create a `.env` file in the root of the project with the following content:
+
+```shell
+OPENAI_API_KEY=<your-key-here>
+```
+
+The CLI will automatically load this file if it exists.
+
+For the AI to write to file, the `bin`-directory must exist in the working directory.
+
+
+## Examples
+
+Use AutoGPT to plan a romantic trip to Paris
+
+```shell
+python -m agent_chain auto-gpt "Plan a romantic weekend in Paris, write the plan to file"
+```
+
+Use AutoGPT to research lobster-fishing using Ocean Info Hub:
+
+```shell
+python -m agent_chain auto-gpt "Research the best places to fish for lobster around Norway. Prioritize using Ocean Info Hub, but research the Ocean Info Hub graph before attempting any queries. Make a todo-list before anything else. Write results to file"
+```
+
+Use BabyAGI to plan a romantic trip to Paris
+
+```shell
+python -m baby-agi --max-iterations 5 "Plan a romantic weekend in Paris"
+```
--- agent_chain/__main__.py
+++ agent_chain/__main__.py
@@ -3,13 +3,10 @@ import string
 
 import dotenv
 import typer
-from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.experimental import AutoGPT
 from langchain.llms import OpenAI
-from langchain.memory import ChatMessageHistory
-from langchain.schema import messages_to_dict
 from langchain.vectorstores import VectorStore
 from langchain.vectorstores.redis import Redis
 
@@ -21,31 +18,6 @@ app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
-
-
-@app.command()
-def llm(
-    prompt: str,
-    max_tokens: int = 100,
-    openapi_key: str = typer.Argument("openapi-key", envvar="OPENAI_API_KEY"),
-):
-    llm = OpenAI(client="", openai_api_key=openapi_key, max_tokens=max_tokens)
-
-    tools = load_tools(["serpapi"])
-    agent = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True)
-
-    history = ChatMessageHistory()
-    # history.add_user_message("Hello, in this conversation please only use metric units."
-    # " Please make sure to convert all messages to metrics before displaying them to me and drop any"
-    # " alternative representations unless otherwise specified. Confirm with 'OK'")
-    # history.add_ai_message("OK")
-
-    agent.run(input=prompt, chat_history=messages_to_dict(history.messages))
-
-
-@app.command()
 def baby_agi(
     objective: str,
     openapi_key: str = typer.Argument("openapi-key", envvar="OPENAI_API_KEY"),
@@ -53,7 +25,6 @@ def baby_agi(
     max_tokens: int = 100,
     temperature: float = 0.0,
     verbose: bool = False,
-    embedding_size: int = 1536,
 ):
     ObjectFactoryRegistry.add(OpenAIEmbeddings, client="", openai_api_key=openapi_key)
     ObjectFactoryRegistry.add(
@@ -99,7 +70,6 @@ def baby_agi(
 def auto_gpt(
     objective: str,
     openapi_key: str = typer.Argument("openapi-key", envvar="OPENAI_API_KEY"),
-    max_iterations: int = 5,
     max_tokens: int = 1000,
     temperature: float = 0.0,
     verbose: bool = False,
